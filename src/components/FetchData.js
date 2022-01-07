a4d99eaf128e6082710e1c891cf5aebd068d2bc5
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-08 00:58:38 +0100

    Fetching data with useEffect Get one by id

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -1,26 +1,29 @@
-
-import axios from 'axios'
-import React, { useState } from 'react'
-import { useEffect } from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 function FetchData() {
-    const [posts, setPosts] = useState([])
-    
-    useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/posts?_limit=5")
-        .then(res=>{
-            console.log(res.data)
-             setPosts(res.data)
-        })
-        .catch(err=>console.log(err))
-    },[])
 
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(5);
 
-    return (
-        <ul>
-             {posts.map((post)=><li key={post.id}>{post.title}</li>)}
-        </ul>
-    )
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        setPost(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [id]); //you must put the id param inside bracket
+
+  return (
+        <div>
+
+         <input value={id} type='text' onChange={(e)=>setId(e.target.value)}  />
+        <h3>{post.title}</h3>
+        </div>
+  );
 }
 
-export default FetchData
+export default FetchData;
