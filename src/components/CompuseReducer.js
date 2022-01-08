8d1e478535fa3e209c8400fbef099fd9a526fb03
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-08 12:17:18 +0100

    multiple useReduce

--- src/components/CompuseReducer.js
+++ src/components/CompuseReducer.js
@@ -2,27 +2,14 @@ import React, { useReducer } from 'react'
 
 
 const initialCount= {
-    countValue1:0,
-    countValue2:10
-
+    countValue:0
 }
 const Reducer=(count, action)=>{
     switch (action.type) {
-        case 'increment1':
-            return {...count,countValue1:count.countValue1+action.val}
-        case 'decrement1':
-            return {...count,countValue1:count.countValue1-action.val}
-
-        case 'increment2':
-            return {...count,countValue2:count.countValue2+action.val}
-        case 'decrement2':
-            return {...count,countValue2:count.countValue2-action.val}
-
-
-        case 'reset1':
-            return {...count,countValue1:0}
-        case 'reset2':
-            return {...count,countValue2:0}
+        case 'increment':
+            return {countValue:count.countValue+action.val}
+        case 'decrement':
+            return {countValue:count.countValue-action.val}
         case 'reset':
                 return initialCount
         default:
@@ -31,25 +18,21 @@ const Reducer=(count, action)=>{
 }
 
 function CompuseReducer() {
-    const [count, action] = useReducer(Reducer, initialCount)
+    const [count1, action1] = useReducer(Reducer, initialCount)
+    const [count2, action2] = useReducer(Reducer, initialCount)
     return (
         <div>
-             <p>{count.countValue1}</p>
-            <button onClick={()=>action({type:'increment1',val:1})}>increment 1</button>
-            <button onClick={()=>action({type:'decrement1',val:1})}>decrement 1</button>
-
-            <button onClick={()=>action({type:'increment1',val:2})}>increment 2</button>
-            <button onClick={()=>action({type:'decrement1',val:2})}>decrement 2</button>
-            <button onClick={()=>action({type:'reset1'})}>Reset1</button>
+             <p>{count1.countValue}</p>
+            <button onClick={()=>action1({type:'increment',val:1})}>increment 1</button>
+            <button onClick={()=>action1({type:'decrement',val:1})}>decrement 1</button>
+            <button onClick={()=>action1({type:'reset'})}>reset</button>
             <hr/>
 
-            <p>{count.countValue2}</p>
-            <button onClick={()=>action({type:'increment2',val:10})}>increment 10</button>
-            <button onClick={()=>action({type:'decrement2',val:10})}>decrement 10</button>
-            <button onClick={()=>action({type:'reset2'})}>Reset2</button>
-
-            <hr/>
-            <button onClick={()=>action({type:'reset'})}>Reset All</button>
+            <p>{count2.countValue}</p>
+            <button onClick={()=>action2({type:'increment',val:10})}>increment 10</button>
+            <button onClick={()=>action2({type:'decrement',val:10})}>decrement 10</button>
+            <button onClick={()=>action2({type:'reset'})}>reset</button>
+  
 
         </div>
     )
