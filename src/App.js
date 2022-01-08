2ee5916e011b5af34acdc194f94fe35668952440
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-08 14:25:25 +0100

    Fetching-with-useState-useEffect

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import React, {useReducer } from "react";
+import React from "react";
 import "./App.css";
-import ComponentA from "./components/ComponentA";
-import ComponentB from "./components/ComponentB";
+import FetchData from "./components/FetchData";
 
 
-export const CountContext = React.createContext();
 
-const initialState = 0;
-
-const reducer = (state, action) => {
-  switch (action) {
-    case "increment":
-      return state + 1;
-    case "decrement":
-      return state - 1;
-    case "reset":
-      return initialState;
-    default:
-      return state;
-  }
-};
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialState);
-
+  
   return (
     <div className="App">
-       <p>{count}</p>
-      <CountContext.Provider value={{count:count,dispatch:dispatch}}>
-          <ComponentA />
-          <ComponentB />
-        
-      </CountContext.Provider>
+      <FetchData/>
     </div>
   );
 }
