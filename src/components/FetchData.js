9b0a4cc1727d34bab5cec54065513e1038945f6d
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-08 01:08:09 +0100

    Fetching data with useEffect Get by click

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -5,22 +5,28 @@ import { useEffect } from "react";
 function FetchData() {
 
   const [post, setPost] = useState({});
-  const [id, setId] = useState(5);
+  const [id, setId] = useState(1);
+  const [idFromButton, setidFromButton] = useState(id);
 
+  const clickHandler=()=>{
+    setidFromButton(id)
+  }
+  
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButton}`)
       .then((res) => {
         console.log(res.data);
         setPost(res.data);
       })
       .catch((err) => console.log(err));
-  }, [id]); //you must put the id param inside bracket
+  }, [idFromButton]); //you must put the id param inside bracket
 
   return (
         <div>
 
          <input value={id} type='text' onChange={(e)=>setId(e.target.value)}  />
+         <button onClick={clickHandler}>click me</button>
         <h3>{post.title}</h3>
         </div>
   );
